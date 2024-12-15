a3360bbd0a00c70ed033f70c9868bb255bee7cee
David <70939795+pezheadpunk@users.noreply.github.com>
2024-12-15 15:52:40 -0700

    Delete leetcode/18_climbing_stairs.py

--- leetcode/18_climbing_stairs.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n <= 1:
-            return n
-        a, b = 1, 1
-        for _ in range(2, n + 1):
-            a, b = b, a + b
-        return b
