afa24925a515f084ea1670101e30f180fbf37008
David <70939795+pezheadpunk@users.noreply.github.com>
2025-02-03 11:30:43 -0700

    Update 236_Lowest_Common_Ancestor_of_a_Binary_Tree.py

--- 236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
+++ 236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -8,18 +8,19 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    '''
-    Uses recursion to do a Depth First Search of the binary tree and if the
-    root passed in is None or is one of the TreeNodes passed in, we return
-    the root whether it is None or the p or q TreeNode becaus then we use 
-    that result later on when determining what to return as the lowest
-    common ancestot. If both left and right are not None then the current
-    TreeNode is the common ancestor and we return it. If there only one of
-    left and right is not None, then we return the one that is not None.
-    Runtime: O(n) -> traversing the binary tree
-    Space: O(1) -> No extra space is used
-    '''
+    '''Soution Class'''
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        '''
+        Uses recursion to do a Depth First Search of the binary tree and if the
+        root passed in is None or is one of the TreeNodes passed in, we return
+        the root whether it is None or the p or q TreeNode becaus then we use 
+        that result later on when determining what to return as the lowest
+        common ancestot. If both left and right are not None then the current
+        TreeNode is the common ancestor and we return it. If there only one of
+        left and right is not None, then we return the one that is not None.
+        Runtime: O(n) -> traversing the binary tree
+        Space: O(1) -> No extra space is used
+        '''
         if root is None or root is p or root is q:
             return root
         left = self.lowestCommonAncestor(root.left, p, q)
@@ -27,3 +28,4 @@ class Solution:
         if left and right:
             return root
         return left or right
+        
