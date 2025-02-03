c7550cab315aa160c0b5c44d9ad2df45fb30ad62
David <70939795+pezheadpunk@users.noreply.github.com>
2025-02-03 11:32:48 -0700

    Update 791_Custom_Sort_String.py

--- 791_Custom_Sort_String.py
+++ 791_Custom_Sort_String.py
@@ -1,18 +1,19 @@
 '''791. Custom Sort String'''
 
 class Solution:
-    '''
-    Creates a dictionary of the order string with each character as a key 
-    and its index in the string as the value while also adding blank 
-    strings to the indexes of the the result list where those characters
-    might be placed. Then iterates over the s string's characters and if 
-    they are in the order dictionary, adds that character to the string in
-    the index that matched the dictionary's value. Then joins the result 
-    list to one string and returns it.
-    Runtime: O(n) -> iterating over the s string
-    Space: O(n) -> result list
-    '''
+    '''Solution Class'''
     def customSortString(self, order: str, s: str) -> str:
+        '''
+        Creates a dictionary of the order string with each character as a key 
+        and its index in the string as the value while also adding blank 
+        strings to the indexes of the the result list where those characters
+        might be placed. Then iterates over the s string's characters and if 
+        they are in the order dictionary, adds that character to the string in
+        the index that matched the dictionary's value. Then joins the result 
+        list to one string and returns it.
+        Runtime: O(n) -> iterating over the s string
+        Space: O(n) -> result list
+        '''
         result = []
         order_dict = {}
         for index, char in enumerate(order):
@@ -24,3 +25,4 @@ class Solution:
             else:
                 result.append(char)
         return "".join(result)
+        
