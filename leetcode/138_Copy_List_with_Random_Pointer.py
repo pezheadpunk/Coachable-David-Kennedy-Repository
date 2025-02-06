e94446982da23cfbd6f2063f7f4ebdb2267c9d88
David <70939795+pezheadpunk@users.noreply.github.com>
2025-02-06 15:20:38 -0700

    Update 138_Copy_List_with_Random_Pointer.py

--- leetcode/138_Copy_List_with_Random_Pointer.py
+++ leetcode/138_Copy_List_with_Random_Pointer.py
@@ -1,9 +1,9 @@
 '''138. Copy List with Random Pointer'''
 # Definition for a Node.
 class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+    def __init__(self, x: int, nxt: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
-        self.next = next
+        self.next = nxt
         self.random = random
 
 class Solution:
